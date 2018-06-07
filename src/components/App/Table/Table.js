f25e3c860abbe22223fcc451b7d8fde4eaf2ad83
azmul <azmulg@gmail.com>
2018-06-07 08:17:01 +0600

    project2 ---> table data show dynamically due to change one sample data to another

--- src/components/App/Table/Table.js
+++ src/components/App/Table/Table.js
@@ -4,20 +4,21 @@ import {connect} from 'react-redux';
 
 import './Table.css';
 import {fetchdatas} from '../../../redux/actions/tableActions';
-import TableRow from './TableRow/TableRow';
+//import TableRow from './TableRow/TableRow';
 
 class Table extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             datas: [],
+            tableHeaderData:{},
             index: -1,
             done: false
         };
     }
     componentWillMount = () =>{
         this.props.fetchdatas().then(
-            ()=>{this.setState({datas: this.props.datas})},
+            ()=>{this.setState({datas: this.props.datas, tableHeaderData: this.props.datas[1]})},
             (err) =>{console.log(err)}
         );
     }
@@ -25,33 +26,64 @@ class Table extends Component {
        this.setState({done: true, index: index})
     }
     dataShow = () =>{
-        //console.log(this.state.index);
         const data = this.state.datas[this.state.index];
-        console.log(data);
     }
+    onSort = (event, sortKey)=>{
+        const datas = this.state.datas;
+      
+        datas.sort(function(a, b) {
+            if (a[sortKey] > b[sortKey]) {
+            return 1;
+            }
+            if (a[sortKey] < b[sortKey]) {
+            return -1;
+            }
+            return 0;
+        });
+        this.setState({datas:datas})
+      }
     render() {
         const {datas} = this.props;
+        const {tableHeaderData} = this.state;
+        
+        let tableHeader=[];
+        if(tableHeaderData){
+            tableHeader = Object.keys(tableHeaderData);
+        }
         const tableItems = datas.length > 0 ? (
-            <tbody>
-                {datas.map((data, index)=><TableRow key={index} index={index} data={data} edit={this.editHandaler} />)}
-            </tbody>     
+           <div>
+            <table className="data-table">
+             <thead>
+                 <tr className="data-table-header">
+                 {
+                     tableHeader.map((data,index)=>(
+                        <th key={index} onClick={event => this.onSort(event, data)}>{data}</th>
+                    )) 
+                 }
+                 </tr>
+             </thead>
+             <tbody>
+                 {datas.map((data, index) =>
+                 (
+                     <tr key={index} data-item={data}>
+                     <td contenteditable="true" data-title={data[tableHeader[0]]}>{data[tableHeader[0]]}</td>
+                     <td contenteditable="true" data-title={data[tableHeader[1]]}>{data[tableHeader[1]]}</td>
+                     <td contenteditable="true" data-title={data[tableHeader[2]]}>{data[tableHeader[2]]}</td>
+                     <td contenteditable="true" data-title={data[tableHeader[3]]}>{data[tableHeader[3]]}</td>
+                     </tr>
+                 )
+                 )}
+             </tbody>
+           </table>
+           <button className="table-row-data-show" onClick={this.dataShow}>Show</button>
+          </div>     
         ): (<div className="no-data">No Data Available</div>)
 
         return (
-            <div>
-              <table>   
-                <tbody>
-                    <tr>
-                        <th>company_name</th>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>fda_date_approved</th>
-                    </tr>
-                    {tableItems}
-                 </tbody>  
-              </table> 
-              <button onClick={this.dataShow}>Show</button>
-            </div>
+          <div>
+              {tableItems}
+          </div>
+            
         );
     }
 }
