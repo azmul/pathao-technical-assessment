3ace4fcb8f7fdb14608562fc5c19552314134063
azmul <azmulg@gmail.com>
2018-06-07 07:29:40 +0600

    project1 --> Initially done

--- Project1/script.js
+++ Project1/script.js
@@ -1,32 +1,84 @@
-let animationDiv = document.querySelector('#animation');
-let AnimaionBOX = {
-    goBoxUpDown: true,
-    goBoxLeftRight: false,
-    side: 1,
-    getbodyWidth: () => document.querySelector('body').offsetWidth,
-    getbodyHeight: () => document.querySelector('body').offsetHeight,
-    animateInterval(animationDiv) {
-        if (animationDiv) {
-            this.startAnimation(animationDiv)
-        }
-    },
-    startAnimation(animationDiv) {
-        setInterval(() => {
-            if (this.goBoxUpDown) {
-                animationDiv.style.top = (this.side === 3) ? 0 : this.getbodyHeight() - 100
-                this.goBoxLeftRight = true
-                this.goBoxUpDown = false
-                this.side += 1
-            } else {
-                animationDiv.style.left = (this.side === 4) ? 0 : this.getbodyWidth() - 100
-                this.goBoxUpDown = true
-                this.goBoxLeftRight = false
-                this.side += 1
-            }
-
-            if (this.side > 4) this.side = 1;
-        }, 1000)
+class AnimaionBOX{
+    constructor(){
+        this.animationDiv = document.querySelector('#animation');
+        this.animationDiv.style.width = "100px";
+        this.animationDiv.style.height = "100px";
+        this.animationDiv.style.position = "absolute";
+        this.animationDiv.style.border = "1px solid blue";
+        this.animationDiv.style.top = "50px";
+        this.Right = true;
+        this.Left = false;
+        this.Bottom = true;
+        this.Top = false;
+        this.side = 1;
+        this.BodyWidth = () => document.querySelector('body').offsetWidth;
+        this.BodyHeight = () => document.querySelector('body').offsetHeight;
     }
+
+    animateInterval() {
+        if (this.animationDiv ) {
+            let top = this.animationDiv.offsetTop;
+            let left = this.animationDiv.offsetLeft;
+            setInterval(() => {
+                if(this.Bottom === true){
+                    if(top <= (this.BodyHeight()-100)){
+                        this.animationDiv.style.top =  top;
+                        top += 10; 
+                        if(this.Right === true){
+                           left += 10;
+                           this.animationDiv.style.left =  left;
+                           if(left >= (this.BodyWidth()-100)){
+                             this.Right = false;
+                             this.Left = true;
+                           }
+                        }
+                        if(this.Left === true){
+                            left -= 10;
+                            this.animationDiv.style.left =  left;
+                            if(left <= 0){
+                              this.Right = true;
+                              this.Left = false;
+                            }
+                         }
+                    }else{
+                        this.Bottom = false;
+                        this.Top = true;
+                        top -= 10; 
+                    } 
+                }
+
+                if(this.Top === true){
+                    if(top >= 0){
+                        this.animationDiv.style.top =  top;                   
+                        top -= 10;
+                        if(this.Right === true){
+                            left += 10;
+                            this.animationDiv.style.left =  left;
+                            if(left >= (this.BodyWidth()-100)){
+                              this.Right = false;
+                              this.Left = true;
+                            }
+                         }
+                         if(this.Left === true){
+                             left -= 10;
+                             this.animationDiv.style.left =  left;
+                             if(left <= 0){
+                               this.Right = true;
+                               this.Left = false;
+                             }
+                          }
+                    }else{
+                        this.Bottom = true;
+                        this.Top = false;
+                        top += 10; 
+                    } 
+                }
+
+            }, 1000)
+        }
+    }
+
 }
 
-AnimaionBOX.animateInterval(animationDiv);
+let animaionBOX = new AnimaionBOX();
+animaionBOX.animateInterval();
